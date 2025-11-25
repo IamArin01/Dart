e2c609c26b822084367264c963d4087572c57075
Parmeet Singh Saluja <125836256+IamArin01@users.noreply.github.com>
2025-11-25 23:07:47 +0530

    Create control-flow.dart

---
+++ control-flow.dart
@@ -0,0 +1,18 @@
+void main(){
+
+//same as every lang
+String jiyan = 'main hu bada takatwar';
+if(jiyan.isNotEmpty) print('mera gala hai bahut sureela!');
+//use have to explicity make expression to bool statement to check if value is truthy or falsy
+//.isEmpty and .isNotEmpty is used to make values bool
+
+//if we pass null to a certain string
+String? doraCake;
+
+//it defaults the String to falsy
+if(doraCake != null);
+
+
+//control flow is same as any other languages
+
+}
