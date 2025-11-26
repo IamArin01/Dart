ad6f642fbccb04d1edb28b93e77bd3e0f5743c65
Parmeet Singh Saluja <125836256+IamArin01@users.noreply.github.com>
2025-11-26 23:38:48 +0530

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -1,7 +1,7 @@
 void main() {
   int a = 10;
   double b = 20.5;
-  String c = "Hello, Dart!";
+  String c = 'Hello, Dart!';
   bool d = true;
 
   print("Integer: $a double: $b String: $c Boolean: $d");
@@ -11,6 +11,7 @@ void main() {
   //is in an instance-of operator
   print(d is int);
 
+  //convention is to take String values with single-quotes
 
   String t = 'hemsworth';
   print('oh my $t !!\n ${t is String}');
@@ -20,9 +21,26 @@ void main() {
   //r works as same as in f-strings for python
   print(r"$ is same as " "$dollar");
 
-  //final keyword means value cant be changed
+  //final keyword means value cant be changed or reassigned letter
   final bro = 'i am good';
+  final int speed = 45; //with datatype
 
   //const is better than final and improve performance and is immutable compile time constant
   const int marks = 98;
+
+
+  //dynamic variable
+  
+  var hey = 'Howdy';
+  print(hey);
+  hey = 45; //will throw error
+  print(hey);
+
+  //is like var but can be reassigned whenever we want
+  dynamic harry = 'potter';
+  print(harry);
+  harry = 31;
+  print(harry); //will run 
+
+    
 }
