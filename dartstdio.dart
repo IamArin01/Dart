3bd43586ec77ebf9a25f09a6d8d9b7a25c05c98c
Parmeet Singh Saluja <125836256+IamArin01@users.noreply.github.com>
2025-11-27 23:58:06 +0530

    stdio

---
+++ dartstdio.dart
@@ -0,0 +1,13 @@
+import dart:io  //import dart:io
+void main(){
+  //readLineSync
+  String? name = stdin.readLineSync();
+
+  print("your name: $name");
+  //for int
+  int? n = int.parse(stdin.readLineSync)!);
+  print("your number: $n");
+  stdout,write("hey there");//wont get to newline after execution
+  
+  
+}
