c42f4ce4fe1a9d8b98342d3e740535eb50b4aa5b
Parmeet Singh Saluja <125836256+IamArin01@users.noreply.github.com>
2025-11-29 00:42:06 +0530

    functions updated

--- functions.dart
+++ functions.dart
@@ -4,19 +4,37 @@ void main(){
     return 'Hello Hello';//without defining return type
   }
 
+  print(sayHello());
   
   String mul5(int a){
     return 'five multiply $a is ${a*5}'; //defining it here
   }
 
-  mul(45);
+  print(mul5(45));
 
 
 
   //named parameters //used in flutter
 
-  namedParas({ required int a, int b = 4}){// datatype of parameters shouldnt be null
+  namedParams({ required int a, int b = 4}){// datatype of parameters shouldnt be null
     //should use required keyword  or initail it beforehand
     return a-b;
     }
+
+  print(namedParams(a: 8, b:2));// should mention name of arguments here
+
+
+
+// one liner Arrow functions
+
+  div5(double g) => '$g divide by 5 equals to ${g/5}';
+  print(div5(42));
+
+//Anonymous Functions
+//callIt({
+//  () => 'hey there'
+//});
+  
+
+  
 }
